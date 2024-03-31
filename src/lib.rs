e978eeb8661967473982ca1dd1ccec2a1ebd4053
Rafael Costa <rafael@rafaelcosta.me>
2024-04-01 00:09:07 +0200

    Updates server listen export

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,24 @@
-use std::net::UdpSocket;
+mod server;
 
+use std::net::UdpSocket;
 use neon::prelude::*;
 use quiche::ConnectionId;
+use server::QUICServerImpl;
+use server::QUICServer;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
 fn listen(mut cx: FunctionContext) -> JsResult<JsBoolean> {
-    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
+    let server = QUICServerImpl {};
+    let thread = std::thread::spawn(move || {
+        server.listen();
+    });
 
-    let _ = config.set_application_protos(&[b"quic-n-dirty"]);
+    let _ = thread.join();
 
-    config
-        .load_priv_key_from_pem_file("./key.pem")
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
-    config
-        .load_cert_chain_from_pem_file("./cert.pem")
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
-
-    let socket = UdpSocket::bind("localhost:34254")
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
-
-    // Receives a single datagram message on the socket. If `buf` is too small to hold
-    // the message, it will be cut off.
-    let mut buf = [0; 2048];
-
-    let (amt, src) = socket
-        .recv_from(&mut buf)
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
-
-    let mut key: [u8; 32] = rand::random(); // up to 32 elements
-
-    let local_addr = socket.local_addr().unwrap();
-
-    // Server connection.
-    let scid: ConnectionId = ConnectionId::from_ref(&key);
-    let mut conn = quiche::accept(&scid, None, local_addr, src, &mut config)
-        .map_err(|e| JsError::error(&mut cx, e.to_string()).unwrap_err())?;
-
-    loop {
-        let (read, from) = socket.recv_from(&mut buf).unwrap();
-
-        let recv_info = quiche::RecvInfo { from, to: local_addr };
-
-        let read = match conn.recv(&mut buf[..read], recv_info) {
-            Ok(v) => v,
-
-            Err(quiche::Error::Done) => {
-                // Done reading.
-                println!("Done reading!");
-                0 as usize
-            }
-
-            Err(e) => {
-                // An error occurred, handle it.
-                println!("Error reading: {}", e.to_string());
-                0 as usize
-            }
-        };
-
-        println!("Done! Read: {}", read);
-    }
+    return Ok(cx.boolean(true));
 }
 
 #[neon::main]
